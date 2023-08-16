c95d9c1ece7f8a0693788110a8a8dd75d19a4797
2beemic <2beemic@gmail.com>
2023-08-17 04:01:29 +0900

    最後かな?

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // pages/index.js
 import Link from "next/link";
-import { client } from "../libs/client";
+import { client } from "@/libs/client";
 
 export default function Home({ blog }) {
     return (
